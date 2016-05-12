73f1387bf1d28ebd5d22da0ceeeca65b6bd0dd6a
Gary McGhee <gary@buzzware.com.au>
2016-05-12 11:50:42 +0800

    Fody binding bug

--- XAMLSamples/XamlSamples/XamlSamples/ListViewDemoPage.xaml.cs
+++ XAMLSamples/XamlSamples/XamlSamples/ListViewDemoPage.xaml.cs
@@ -1,16 +1,46 @@
-﻿using System;
+﻿// This exposes a bug in Fody. When the ListViewPage appears, it should show the list of colours, and it does without the Fody OnPropertyChanged implementation. With Fody, nothing happens.
+
+#define FODY			// comment out this line to see it work with the manually implemented OnPropertyChanged
+
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PropertyChanged;
 
-namespace XamlSamples
-{
-    public partial class ListViewDemoPage
+
+namespace XamlSamples {
+
+	[ImplementPropertyChanged]
+  public partial class ListViewDemoPage {
+    public ListViewDemoPage()
     {
-        public ListViewDemoPage()
-        {
-            InitializeComponent();
-        }
+				BindingContext = this;
+        InitializeComponent();
+			ListViewItems = new ObservableCollection<NamedColor> (NamedColor.All);
     }
+
+		#if FODY
+		public ObservableCollection<NamedColor> ListViewItems;
+		#else    
+		private ObservableCollection<NamedColor> listItems;
+		public ObservableCollection<NamedColor> ListViewItems {
+			get {
+				return listItems;
+			}
+			set {
+				listItems = value;
+				OnPropertyChanged (nameof (ListViewItems));
+			}
+		}
+		#endif
+
+		void Handle_Clicked (object sender, System.EventArgs e)
+		{
+			ListViewItems = new ObservableCollection<NamedColor> (NamedColor.All);
+		}
+
+	}
 }
